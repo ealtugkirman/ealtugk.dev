e243d81a9eba4ade003c080be49c8972cbb05e6a
emrealtugkirman <ealtugkirman@gmail.com>
2023-01-22 18:55:03 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import Al2 from "./components/Al2"
+
+function App() {
+  return (
+    <div className="flex items-center font-semibold justify-center bg-amber-400 h-screen" >
+      <div >
+        <Al2 />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Al2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Me from "../assets/me.jpg"
+
+function Al2() {
+  return (
+    <div className='mx-auto text-center'>
+        <h1>Hi i'm Al2</h1>
+        <h2>this is will be my personal site</h2>
+        <h3>just not yet ehe ehe</h3>
+        <img className='rounded-full animation-spin' src={Me} alt="thisisemrealtugkirmanehehe" />
+    </div>
+  )
+}
+
+export default Al2
